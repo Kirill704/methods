201a57bdc9047bec76c8b25c934dee86681720db
Kirill704 <zkirillw@yandex.ru>
2025-11-16 20:05:55 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,73 @@
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static String yearType(int year) {
+        String answer;
+        if (year >= 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+            answer = " год - високосный год";
+        } else {
+            answer = " год - невисокосный год";
         }
+        return answer;
+    }
+
+    public static String preferOs(byte clientOS, short clientDeviceYear) {
+        String prefer;
+        int currentYear = LocalDate.now().getYear();
+        if (clientDeviceYear < currentYear && clientOS == 0) {
+            prefer = "Установите облегченную версию приложения для iOS по ссылке";
+        } else if (clientDeviceYear < currentYear && clientOS == 1) {
+            prefer = "Установите облегченную версию приложения для Android по ссылке";
+        } else if (clientDeviceYear >= currentYear && clientOS == 0) {
+            prefer = "Установите версию приложения для iOS по ссылке";
+        } else if (clientDeviceYear >= currentYear && clientOS == 1) {
+            prefer = "Установите версию приложения для Android по ссылке";
+        } else {
+            prefer = "неверно указана OS";
+        }
+        return prefer;
+    }
+
+    public static String delivery(int deliveryDistance) {
+        String message;
+        if (deliveryDistance <= 100) {
+            byte days = 3;
+            if (deliveryDistance < 60) {
+                days--;
+                if (deliveryDistance < 20) {
+                    days--;
+                }
+            }
+            message = "Потребуется дней: " + days;
+        } else {
+            message = "доставки нет";
+        }
+        return message;
+    }
+
+    public static void main(String[] args) {
+
+//задача 1
+        System.out.println("задача 1");
+
+        int year = 2021;
+        System.out.println(year + yearType(year));
+
+//задача 2
+        System.out.println("задача 2");
+
+        byte clientOS = 1;
+        short clientDeviceYear = 2015;
+        System.out.println(preferOs(clientOS, clientDeviceYear));
+
+//задача 3
+        System.out.println("задача 3");
+
+        int deliveryDistance = 95;
+        System.out.println(delivery(deliveryDistance));
+
     }
 }
